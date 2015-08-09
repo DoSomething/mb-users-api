44869d563c9e33adc4d0f1971f8f671f0677fe22
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-09 17:58:41 -0400

    Check for required params

--- mb-users-api-server.js
+++ mb-users-api-server.js
@@ -2,6 +2,7 @@ var express = require('express')
     , mongoose = require('mongoose')
     , User = require('./lib/user')
     , Users = require('./lib/users')
+    , UsersCursor = require('./lib/usersCursor')
     , UserBanned = require('./lib/userBanned')
     , mb_config = require(__dirname + '/config/mb_config.json')
     , dslogger = require('./lib/dslogger');
@@ -159,7 +160,13 @@ app.get('/user', function(req, res) {
  */
 app.get('/users', function(req, res) {
 
-  if (!req.body.type == 'realTime') {
+  if (req.query.type == 'realTime') {
+    if (req.query.direction === undefined) {
+      response.send(400, 'The "direction" needs to be defined.');
+    }
+    if (req.query.pageSize === undefined) {
+      response.send(400, 'The "pageSize" needs to be defined.');
+    }
     var usersCursor = new UsersCursor(userModel);
     usersCursor.get(req, res);
   }
